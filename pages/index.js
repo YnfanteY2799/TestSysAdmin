ae9a96914f05572399e2dc75a802df73b35ce963
AlamV01 <118229902+AlamV01@users.noreply.github.com>
2022-11-28 13:34:31 -0400

    +updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import Image from "next/image";
+<<<<<<< Updated upstream
 
 export default function Home() {
   return <>Hola</>;
 }
+=======
+//import styles from "../styles/Home.module.css";
+import Navbar from "../public/Navbar/Navbar.jsx";
+
+export default function Home() {
+  return (
+    <div>Hola mundo!</div>
+  )
+}
+>>>>>>> Stashed changes
